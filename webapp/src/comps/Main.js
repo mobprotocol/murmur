52236a6eae2a6f88e1152e1598b779706f758f7f
Sean Pollock <seanpollock3344@gmail.com>
2017-10-29 18:05:47 -0400

    roboto font

--- webapp/src/comps/Main.js
+++ webapp/src/comps/Main.js
@@ -3,9 +3,19 @@ import React, { Component } from 'react'
 export default class Main extends Component {
   render() {
     return (
-      <div style={{ backgroundColor: '#F5F5F5', width: '100%', height: '100%', borderRadius: 5}}>
+      <div style={mainStyle}>
         <h1>murmur</h1>
       </div>
     )
   }
+}
+
+const mainStyle = {
+  backgroundColor: '#F5F5F5',
+  width: '100%',
+  height: '100%',
+  borderRadius: 5,
+  display: 'flex',
+  color: '#696969',
+  fontFamily: 'roboto'
 }
