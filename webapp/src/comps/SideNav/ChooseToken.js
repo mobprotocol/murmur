dfd7dfdb4339ba3990614bd7c9af5b335ea6eb34
Sean Pollock <seanpollock3344@gmail.com>
2017-10-30 14:56:14 -0400

    adding moc images and sidenav

---
+++ webapp/src/comps/SideNav/ChooseToken.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+export default class ChooseToken extends Component {
+  render() {
+    return (
+      <div style={chooseTokenStyle}>
+        <div style={tileStyle}>
+          <h3>Buy</h3>
+          <img src='./src/imgs/mob_logo.png' />
+        </div>
+        <div style={tileStyle}>
+          <h3>Sell</h3>
+          <img src='./src/imgs/ethereum_logo.png' />
+        </div>
+      </div>
+    )
+  }
+}
+
+const chooseTokenStyle = {
+  display: 'flex',
+  textAlign: 'center'
+
+}
+
+const tileStyle = {
+  width: '50%',
+  height: 100,
+}
+
+
+const tabsStyle = {
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'row'
+}
--- webapp/src/comps/SideNav/SideNav.js
+++ webapp/src/comps/SideNav/SideNav.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import ChooseToken from './ChooseToken'
 
 export default class SideNav extends Component {
   render() {
     return (
       <div style={sideNavStyle}>
-
+        <ChooseToken />
       </div>
     )
   }
